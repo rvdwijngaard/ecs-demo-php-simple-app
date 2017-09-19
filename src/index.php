e1c61200aacbc87663605a1e6f2527f15e8def6e
Ron van der Wijngaard <rhvdwijngaard@gmail.com>
2017-09-19 19:27:17 +0200

    Update index.php

--- src/index.php
+++ src/index.php
@@ -19,6 +19,7 @@
                 <h2>Congratulations</h2>
                 <p>Your PHP application is now running on a container in Amazon ECS.</p>
                 <p>o Yeah this is cool</p>
+                <p>This should be a go service</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
                 <?php
                         $myfile = fopen("/var/www/my-vol/date", "r") or die("");
